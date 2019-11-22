bb8cc0e1db3058cd0b3c10109afd1f9a8c4fec59
pjainxido <pjane123@naver.com>
2019-11-22 22:22:44 +0900

    Add Opacity option on pixelCell.jsx

--- src/components/PixelCell.jsx
+++ src/components/PixelCell.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const GRID_INITIAL_COLOR = '#313131';
-
+const GRIT_INITIAL_OPACITY = 0.5;
 export default class PixelCell extends React.Component {
   shouldComponentUpdate(nextProps) {
-    const keys = ['color', 'width'];
+    const keys = ['color', 'width', 'opacity'];
     const isSame = keys.every(
       key => this.props.cell[key] === nextProps.cell[key]
     );
@@ -12,14 +12,15 @@ export default class PixelCell extends React.Component {
   }
   render() {
     const {
-      cell: { color, width },
+      cell: { color, width, opacity },
       id,
       drawHandlers: { onMouseDown, onMouseOver }
     } = this.props;
     const styles = {
       width: `${width}%`,
       paddingBottom: `${width}%`,
-      backgroundColor: color || GRID_INITIAL_COLOR
+      backgroundColor: color || GRID_INITIAL_COLOR,
+      opacity: opacity || GRIT_INITIAL_OPACITY
     };
 
     return (
